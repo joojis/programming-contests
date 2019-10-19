318775ae0117c78f581a4db4634dee82ea4ab5c9
JinGyeong Jeong <jjgjoojis@gmail.com>
2019-10-19 14:48:35 +0900

    Update algospot/BOGGLE
    It's not solved yet.

--- algospot/BOGGLE/source.rs
+++ algospot/BOGGLE/source.rs
@@ -13,22 +13,82 @@ fn read_string() -> String {
 }
 
 // TODO
-fn has_word() -> bool {
+fn has_word(board: &[[char; 5]; 5], word: &str) -> bool {
+    assert!(word.len() <= 10, "{}", word);
+
+    let mut dynamic = [[[false; 10]; 5]; 5];
+
+    for (k, ch) in word.chars().enumerate() {
+        for i in 0..5 {
+            for j in 0..5 {
+                if board[i][j] != ch {
+                    continue;
+                }
+
+                let found = {
+                    if k == 0 {
+                        true
+                    } else {
+                        let mut ret = false;
+                        for ii in i - 1 .. i + 1 {
+                            for jj in j - 1 .. j + 1 {
+                                if ii < 0 || jj < 0 || ii >= 5 || jj >= 5 {
+                                    continue;
+                                } else if ii == i && jj == j {
+                                    continue;
+                                }
+
+                                // println!("dynamic[{}][{}][{}] == {}", ii, jj, k, dynamic[ii][jj][k - 1]);
+                                if (ch == 'R') {
+                                    println!("ii = {} jj = {}", ii, jj);
+                                }
+                                if dynamic[ii][jj][k - 1] == true {
+                                    ret = true;
+                                }
+                            }
+                        }
+                        ret
+                    }
+                };
+                
+                
+                // println!("found={} k={} len={}", found, k, word.len());
+                if found {
+                    dynamic[i][j][k] = true;
+                    if k + 1 == word.len() {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        println!("word ={} k == {}", word, k);
+        for i in 0..5 {
+            for j in 0..5 {
+                print!("{} ", dynamic[i][j][k]);
+            }
+            println!("");
+        }
+    }
+
     false
 }
 
 fn solve() {
-    let mut board: Vec<String> = vec![];
+    let mut board = [['a'; 5]; 5];
 
     for i in 0..5 {
-        board.push(read_string());
+        let string = read_string();
+        assert_eq!(string.len(), 5);
+        for (j, ch) in string.chars().enumerate() {
+            board[i][j] = ch;
+        }
     }
-    println!("{:?}", board);
 
     let num_of_words = read_integer();
     for i in 0..num_of_words {
         let word = read_string();
-        match has_word() {
+        match has_word(&board, &word) {
             true => println!("{} YES", word),
             false => println!("{} NO", word),
         }
